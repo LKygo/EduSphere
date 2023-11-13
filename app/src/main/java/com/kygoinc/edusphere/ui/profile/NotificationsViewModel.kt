32ed449765fbe13a2e3d92ca7cb27d381dbca134
LKygo <leekygo3@gmail.com>
2023-11-13 11:27:08 +0300

    Initial commit

---
+++ app/src/main/java/com/kygoinc/edusphere/ui/profile/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.kygoinc.edusphere.ui.profile
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is profile Fragment"
+    }
+    val text: LiveData<String> = _text
+}
